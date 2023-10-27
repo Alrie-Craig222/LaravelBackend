4d5da17bdf2e680b82c5f4a9f63c2ff9d1dc94b3
Alrie-Craig222 <almariemaestrado05@gmail.com>
2023-10-27 13:44:07 +0800

    Added Migration File and Model for Receipt

---
+++ app/Models/ReceiptModel.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ReceiptModel extends Model
+{
+    use HasFactory;
+    /**
+		*The table associated with the model
+		*
+		*@var string
+		*/
+		protected $table = 'tablename_in_the_database(tableplus)';
+		
+		/**
+		*The primary key associated with the model
+		*
+		*@var string
+		*/
+		protected $primaryKey = 'PrimaryKey_in_table_in_the_database(tableplus)';
+
+		/**
+		*The attributes that are mass assignable
+		*
+		*@var array
+		*/
+		protected $fillable = [
+					customerID,
+					customer_name,
+					imagepath,
+					description,
+					user_id,
+					
+					]
+}
